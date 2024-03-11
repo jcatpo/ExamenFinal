9386d0751bac06b0e38b4b3285cb7f3cdd250066
Jesus Catpo <jcatpoperez@gmail.com>
2024-03-10 23:34:47 -0500

    examen final

---
+++ EF_CATPO/src/main/java/org/cibertec/edu/pe/repository/ICiudadRepository.java
@@ -0,0 +1,8 @@
+package org.cibertec.edu.pe.repository;
+
+import org.cibertec.edu.pe.model.Ciudad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICiudadRepository extends JpaRepository<Ciudad, String> {
+
+}
---
+++ EF_CATPO/src/main/java/org/cibertec/edu/pe/repository/IVentaDetalleRepository.java
@@ -0,0 +1,7 @@
+package org.cibertec.edu.pe.repository;
+
+import org.cibertec.edu.pe.model.VentaDetalle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IVentaDetalleRepository extends JpaRepository<VentaDetalle, Integer> {
+}
